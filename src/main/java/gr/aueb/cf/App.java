5d6b9ae1bb50b84b16e0b628110fc59fbfc8a1bf
Th. Androutsos <a8anassis@gmail.com>
2024-11-09 12:16:09 +0200

    update

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -1,5 +1,6 @@
 package gr.aueb.cf;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,12 +15,17 @@ public class App
 {
     public static void main( String[] args )
     {
+        WebDriverManager.chromedriver().setup();
+
         WebDriver chromeDriver = new ChromeDriver();
         chromeDriver.get("https://tutorialsninja.com/demo/index.php?route=common/home");
 
         chromeDriver.manage().window().maximize();
 
         WebElement searchField =  chromeDriver.findElement(By.name("search"));
+        searchField.click();
+
+        searchField.sendKeys("MacBook");
 
 
 
